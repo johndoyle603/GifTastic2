9aa11a855061d3165b966f7324a116f576fe17e1
John Doyle <johndoyle@gmail.com>
2019-09-03 12:37:04 -0400

    initial commit(folders)

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+let topics = [
+    "meadow soprano",
+    "bada bing strip club",
+    "tony soprano",
+    "Junior soprano",
+    "dr. melfi",
+    "sopranos pork store",
+    "artie bucco",
+];
+console.log(topics);
+console.log("my finger hurts")
