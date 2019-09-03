7c4217a9be6c045fee7c76f0ce39a20b9fe6f139
John Doyle <johndoyle@gmail.com>
2019-09-03 14:59:25 -0400

    homework submit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,15 @@
 let topics = [
-    "meadow soprano",
-    "bada bing strip club",
-    "tony soprano",
-    "Junior soprano",
-    "dr. melfi",
-    "sopranos pork store",
-    "artie bucco",
+    "Tony Soprano",
+    "Junior Soprano",
+    "Christopher Moltisanti",
+    "Paulie Walnuts",
+    "Silvio Dante",
+    "Johnny Sack",
+    "Bobby Bacala",
 ];
-console.log(topics);
-console.log("my finger hurts")
+
+//Need a loop that will assign objects in array to buttons
+
+//Need an onclick event that will load GIFs from GIPHY API to each button
+
+//
